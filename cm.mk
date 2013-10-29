83e0f3fab7bfc862b3514503b329aeb54b166958
hsr <mailtohsr@gmail.com>
2013-10-29 21:52:27 +0530

    Initial Commit

---
+++ cm.mk
@@ -0,0 +1,15 @@
+$(call inherit-product, device/sony/nanhu_ds/full_nanhu_ds.mk)
+
+# Inherit CM common GSM stuff.
+$(call inherit-product, vendor/cm/config/gsm.mk)
+
+# Inherit CM common Phone stuff.
+$(call inherit-product, vendor/cm/config/common_mini_phone.mk)
+
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=C1604_1270-7235 BUILD_FINGERPRINT=Sony/C1604_1270-7235/C1605:4.1.1/11.3.A.2.1/20130409.185106:user/release-keys PRIVATE_BUILD_DESC="C1604-user 4.1.1 JRO03L 20130409.185106 test-keys"
+
+TARGET_SCREEN_HEIGHT := 480
+TARGET_SCREEN_WIDTH := 320
+
+PRODUCT_NAME := cm_nanhu_ds
+PRODUCT_DEVICE := nanhu_ds
---
+++ include/lights.h
@@ -0,0 +1,12 @@
+#define STANDARD_LIGHTS
+
+char const*const RED_LED_FILE          = "/sys/class/leds/red/brightness";
+char const*const GREEN_LED_FILE        = "/sys/class/leds/green/brightness";
+char const*const BLUE_LED_FILE         = "/sys/class/leds/blue/brightness";
+
+char const*const SNS_LED_FILE           = "/sys/class/leds/lm3533-light-sns/rgb_brightness";
+char const*const LCD_BACKLIGHT_FILE     = "/sys/class/leds/lm3533-light-backlight/brightness";
+char const*const BUTTON_BACKLIGHT_FILE  = "/sys/class/leds/lm3533-light-button/brightness";
+
+
+
