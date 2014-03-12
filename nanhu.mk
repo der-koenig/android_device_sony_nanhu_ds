c128046d9dde70f77c2debef12e2a11ec9a85568
Matt Koenig <der_koenig@rocketmail.com>
2014-03-12 03:24:59 +0100

    Cleanup

--- nanhu.mk
+++ nanhu.mk
@@ -40,9 +40,7 @@ PRODUCT_AAPT_PREF_CONFIG := mdpi
 # Configuration scripts
 PRODUCT_COPY_FILES += \
     device/sony/nanhu/config/audio_policy.conf:system/etc/audio_policy.conf \
-    device/sony/nanhu/config/main.conf:system/etc/bluetooth/main.conf \
     device/sony/nanhu/config/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf \
-    device/sony/nanhu/config/init.qcom.bt.sh:system/etc/init.qcom.bt.sh \
     device/sony/nanhu/config/init.device.rc:root/init.device.rc \
     device/sony/nanhu/config/init.recovery.device.rc:root/init.recovery.device.rc \
     device/sony/nanhu/../tamsui-common/prebuilt/logo_M.rle:root/logo.rle
